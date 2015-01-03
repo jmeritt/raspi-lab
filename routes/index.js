ac5e7e8bad4813d7b8e6c42ea7d5f2ea6201fd83
jmeritt <Jaime.Meritt@gmail.com>
2015-01-03 16:00:04 -0500

    Used counter for filename

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ var exec   = require('child_process').exec;
 var express = require('express');
 var router = express.Router();
 
+var counter = 0;
 /* GET home page. */
 router.get('/tests/run', function(req, res) {
 	//exec('pwd', function(error, stdout, stderr){
-	exec('raspistill -o ./public/images/newResults.jpg', function(error, stdout, stderr){
-  		res.redirect(302, '/images/newResults.jpg');
+	var path = '/images/' + counter++ +'.jpg';
+	exec('raspistill -t 0 -o ./public'+path, function(error, stdout, stderr){
+  		res.redirect(302, path);
   	});
 });
 
