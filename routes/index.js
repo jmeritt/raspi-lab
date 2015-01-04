11931c89b87b6a239c1c39108e8e2278dac3d57c
jmeritt <Jaime.Meritt@gmail.com>
2015-01-04 10:15:02 -0500

    Adding gpio flasing while pic is taking

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var exec   = require('child_process').exec;
+var gpio = require('rpi-gpio');
 var express = require('express');
 var router = express.Router();
 
@@ -6,10 +7,23 @@ var counter = 0;
 /* GET home page. */
 router.get('/tests/run', function(req, res) {
 	//exec('pwd', function(error, stdout, stderr){
+	
 	var path = '/images/' + counter++ +'.jpg';
 	exec('raspistill -t 0 -o ./public'+path, function(error, stdout, stderr){
+  		gpio.setup(6, gpio.DIR_OUT, function(){
+			gpio.write(6, true, function(err) {
+        		if (err) throw err;
+        		console.log('Written to pin');
+    		});
+		});
   		res.redirect(302, path);
+  		gpio.write(6, false, function(err) {
+        		if (err) throw err;
+        		console.log('Written to pin');
+    	});
+    	gpio.destroy();
   	});
+
 });
 
 module.exports = router;
